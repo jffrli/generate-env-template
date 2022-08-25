49447a000c88128c4f7ca8fe1f83e1b8c7674479
Jeffrey Li <jli@operto.com>
2022-08-24 17:04:16 -0700

    Completed basic functionaity.

--- generate_env_template.py
+++ generate_env_template.py
@@ -1,5 +1,12 @@
 import sys, re
 
+def find_var_name(l):
+	env_regex = r"=\s*env(\.(str|bool|int|float|decimal|list|dict|json|datetime|date|time|timedelta|url|uuid|log_level|path|enum))?\(\"([a-zA-Z0-9_\-]*)\"\)"
+	match = re.search(env_regex,l)
+	if not match:
+		return None
+	return match.group(3)
+
 def main():
 	if len(sys.argv) != 2:
 		print("Usage: python3 generate_env_template.py [source].py")
@@ -19,44 +26,40 @@ def main():
 		print(e)
 		return
 
-	'''
-	Possibilities:
-		env("x")
-		
-		[CASTS]
-		env.str("x")
-		env.bool("x")
-		env.int("x")
-		env.float("x")
-		env.decimal("x")
-		env.list("x")
-		env.dict("x")
-		env.json("x")
-		env.datetime("x")
-		env.date("x")
-		env.time("x")
-		env.timedelta("x")
-		env.url("x")
-		env.uuid("x")
-		env.log_level("x")
-		env.path("x")
-		env.enum("x")
-	
-		[PREFIX]
-		with env.prefixed("pre"):
-			x = env("x")
-	'''
 	#prefix 
 	prefix_regex = r"^(\s*)with env.prefixed\(\"([a-zA-Z0-9_\-]*)\"\):"
 
-	for i in range(len(lines)):
+	env_variables = []
+
+	i = 0
+	while i < len(lines):
 		l = lines[i]
 
 		prefix_match = re.search(prefix_regex, l)
 		if (prefix_match):
-			indentation = prefix_match.group(1)
-			greater_indent = r''
+			#print(prefix_match.group(1))
+			#print(prefix_match.group(2))
 
+			indentation = prefix_match.group(1)
+
+			greater_indent = r"^"+ indentation+r"\s"
+			i += 1
+			while (re.search(greater_indent, lines[i])):
+				var_name = find_var_name(lines[i])
+	
+				if var_name:
+					env_variables.append(prefix_match.group(2) + var_name)
+				i += 1	
+		else:
+			var_name = find_var_name(l)
+			if var_name:
+				env_variables.append(var_name)
+		i += 1
+
+	env_variables = [s + "=\n" for s in env_variables]
+
+	with open("env.template", "w") as f:
+		f.writelines(env_variables)
 
 if __name__ == '__main__':
 	main()
